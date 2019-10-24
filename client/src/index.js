d400f4f43ef43ccf0409b71919aa42f72278f68e
Magpie <edchavez821@gmail.com>
2019-10-23 23:24:01 -0500

    Conditional firebase config

--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import * as firebase from "firebase/app";
 
-const config = null;
+const firebaseConfig = null;
 
 if (process.env.NODE_ENV === "development") {
-    config = require('./firebase-config')
+    firebaseConfig = require('./firebase-config')
 }
-
-const firebaseConfig = process.env.REACT_APP_FIREBASE_CONFIG ? JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG) : config
+else if (process.env.NODE_ENV === "production") {
+    firebaseConfig = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG)
+}
 console.log(firebaseConfig)
 firebase.initializeApp(firebaseConfig);
 
