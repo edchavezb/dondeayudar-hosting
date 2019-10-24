fc2192ac7fa7ba4e5ce6984b49fcad60889dfaf2
Magpie <edchavez821@gmail.com>
2019-10-23 23:40:59 -0500

    Changed const for var

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import * as firebase from "firebase/app";
 
-const firebaseConfig = null;
+var firebaseConfig = null;
 
+console.log(process.env.REACT_APP_FIREBASE_CONFIG)
 if (process.env.NODE_ENV === "development") {
     firebaseConfig = require('./firebase-config')
-}
-else if (process.env.NODE_ENV === "production") {
+} else if (process.env.NODE_ENV === "production") {
     firebaseConfig = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG)
 }
 console.log(firebaseConfig)
